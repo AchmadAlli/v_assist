58c17774f8828e542a98594d6285af4a204e326c
Arizal <arizalmhmd5@gmail.com>
2018-06-25 23:30:50 +0700

    just for fun

--- index.php
+++ index.php
@@ -107,6 +107,12 @@ $app->post('/webhook', function($request, $response) use ($bot, $pass_signature)
                             $imageMessage = new \LINE\LINEBot\MessageBuilder\ImageMessageBuilder("https://arizalmhmd5.000webhostapp.com/gantungan.jpg", "https://arizalmhmd5.000webhostapp.com/gantungan.jpg");
                             $result = $bot->replyMessage($event['replyToken'], $imageMessage);
                         }
+
+                        if (strtolower($event['message']['text']) == 'tampiltika') // tampilkan gambar
+                        {
+                            $imageMessage = new \LINE\LINEBot\MessageBuilder\ImageMessageBuilder("https://arizalmhmd5.000webhostapp.com/tika.jpg", "https://arizalmhmd5.000webhostapp.com/tika.jpg");
+                            $result = $bot->replyMessage($event['replyToken'], $imageMessage);
+                        }
                     }
                 } else { // jika pc
                     if ($event['message']['type'] == 'text') {
