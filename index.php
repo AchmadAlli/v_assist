c6a658217b61b02ecb67df343033d1638a67aca2
Arizal <arizalmhmd5@gmail.com>
2018-06-25 21:56:15 +0700

    fixing imageMessage

--- index.php
+++ index.php
@@ -102,7 +102,6 @@ $app->post('/webhook', function($request, $response) use ($bot, $pass_signature)
                         if (strtolower($event['message']['text']) == 'tampil') {
                             $imageMessage = new \LINE\LINEBot\MessageBuilder\ImageMessageBuilder("https://myanimelist.cdn-dena.com/images/characters/8/320273.jpg", "https://myanimelist.cdn-dena.com/images/characters/8/320273.jpg");
                             $result = $bot->replyMessage($event['replyToken'], $imageMessage);
-                            return $result->getHTTPStatus() . ' ' . $result->getRawBody();
                         }
                     }
                 }
