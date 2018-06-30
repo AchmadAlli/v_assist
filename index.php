5e171b3ca06b05c998dc148706973f6ef31b429a
Arizal <arizalmhmd5@gmail.com>
2018-06-30 21:59:30 +0700

    nlp beta

--- index.php
+++ index.php
@@ -155,11 +155,6 @@ $app->post('/webhook', function($request, $response) use ($bot, $pass_signature)
                             }
                         }
                     }
-                }    
-
-                if ($profile['userId'] == "U60b9bd48243e6bb5057e6c9ec5cd2bc0") {
-                    $message = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('mas');
-                    $result = $bot->pushMessage('U7297ad52024284a89e83c406b298553c', $message);
                 }
             }
         }
