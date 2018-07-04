a880402b1d59a07cf78a4f2623a4fae5487d44b9
Arizal <arizalmhmd5@gmail.com>
2018-07-04 21:23:40 +0700

    update simple AI

--- simpleAI.php
+++ simpleAI.php
@@ -16,22 +16,57 @@
     <?php
 
     $kategori = [
-        "get nilai" => [
+        "get_nilai" => [
             "tampilkan nilai",
             "tampilkan nilai saya",
             "nilai"
+        ],
+        "jadwal_kegiatan" => [
+            "minta jadwal kegiatan raja brawijaya",
+            "jadwal raja brawijaya apa aja ?",
+            "jadwalnya raja brawijaya"
+        ],
+        "profile_raja_brawijaya" => [
+            "apa itu raja brawijaya",
+            "raja brawijaya itu apaan ?",
+            "raja brawijaya itu ngapain"
+        ],
+        "lokasi_nongkrong" => [
+            "lokasi yang asik di brawijaya",
+            "tempat nongkrong di UB",
+            "tempat belajar yang nyaman di UB"
         ]
     ];
 
     if (isset($_GET['submit'])) {
         $kalimat = strtolower($_GET["kalimat"]);
-        foreach ($kategori as $key => $value) {
-            for ($i=0; $i < count($value); $i++) { 
-                if ($kalimat == $value[$i]) {
-                    echo $key;
+        
+        $kategoriDimaksud = [0, ""];
+        foreach ($kategori as $namaKategori => $kategori) {
+            $kemiripanTerbesarKategori = 0;
+            echo "<ul>";
+            for ($i = 0; $i < count($kategori); $i++) {
+                similar_text($kalimat, $kategori[$i], $kemiripan);
+                if ($kemiripan >= $kemiripanTerbesarKategori) {
+                    $kemiripanTerbesarKategori = $kemiripan;
                 }
             }
+            echo "<li>Kemiripan terbesar kategori $namaKategori : $kemiripanTerbesarKategori %</li>";
+
+            if ($kategoriDimaksud[0] < $kemiripanTerbesarKategori) {
+                $kategoriDimaksud[0] = $kemiripanTerbesarKategori;
+                $kategoriDimaksud[1] = $namaKategori;
+            } else if ($kategoriDimaksud[0] == $kemiripanTerbesarKategori) {
+                $kategoriDimaksud[1] = "Aku bingung";
+            }
+
+            echo "</ul>";
         }
+        if ($kategoriDimaksud[0] < 60) {
+            $kategoriDimaksud[1] = "aku gak ngerti";
+        }
+        echo "kategori yang dimaksud : " . $kategoriDimaksud[1];
+
     }
 
     ?>
